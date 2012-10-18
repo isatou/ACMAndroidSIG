faed9784154c813ab33cf20d054e61d2aa0a4e23
Hery <aeonzamaek@gmail.com>
2012-10-18 17:56:41 -0400

    added ui search elements

--- src/acm/ccny/menuratingapp/MainActivity.java
+++ src/acm/ccny/menuratingapp/MainActivity.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);    
     }
 
     @Override
