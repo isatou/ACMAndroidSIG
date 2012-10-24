8e4f1589eab137fe119f18444d5e47bac418b132
Ian S McBride <ian.s.mcb@gmail.com>
2012-10-24 16:34:51 -0400

    made the search button create an Activity that will process the searching

--- src/acm/ccny/menuratingapp/MainActivity.java
+++ src/acm/ccny/menuratingapp/MainActivity.java
@@ -1,11 +1,16 @@
 package acm.ccny.menuratingapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "acm.ccny.menuratingapp.MESSAGE";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,4 +22,21 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    //listener for search button
+    public void processSearch (View view) {
+    	
+    	//create intent for a new activity
+    	Intent intent = new Intent(this, ProcessSearchActivity.class);
+    	
+    	//get search field's value aka the query
+    	EditText searchField = (EditText) findViewById(R.id.search_field);
+    	String query = searchField.getText().toString();
+    	
+    	//include the query with the intent
+    	intent.putExtra(EXTRA_MESSAGE, query);
+    	
+    	startActivity(intent);
+    	
+    }//function processSearch
 }
