b5aa25683ea3c35ff4286486dc5e400df456985d
Ian S McBride <ian.s.mcb@gmail.com>
2012-10-24 16:37:24 -0400

    added 3 files that were missing from the last commit

---
+++ src/acm/ccny/menuratingapp/ProcessSearchActivity.java
@@ -0,0 +1,16 @@
+package acm.ccny.menuratingapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class ProcessSearchActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
