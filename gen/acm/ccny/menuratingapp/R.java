eb84d1b2db5455af1d5bf386bff1f1b7dce3b4f9
Hery <aeonzamaek@gmail.com>
2012-10-25 18:03:02 -0400

    modified text fields hints

--- gen/acm/ccny/menuratingapp/R.java
+++ gen/acm/ccny/menuratingapp/R.java
@@ -29,10 +29,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040005;
+        public static final int hello_world=0x7f040006;
+        public static final int item_type_text_field_hint=0x7f040004;
+        public static final int location_text_field_hint=0x7f040003;
         public static final int menu_settings=0x7f040001;
-        public static final int search_bar_hint=0x7f040003;
-        public static final int search_button_text=0x7f040004;
+        public static final int search_button_text=0x7f040005;
         public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
